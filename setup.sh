8e59ce0f21c4cc32e5022624ea83944874fa0107
Dan Snow <dfsnow@uchicago.edu>
2018-05-30 21:06:49 -0500

    Dircolors and bash

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ script_home="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 params="-sf"
 
 # Install vim-plug and tmux config
-curl -fLo ./.vim/autoload/plug.vim --create-dirs \
+curl -fLo $script_home/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 git clone https://github.com/gpakosz/.tmux.git .tmux
@@ -13,15 +13,16 @@ mv .tmux/.tmux.conf ./.tmux.conf
 rm -rf .tmux
 
 # Create symlinks to all files and folders
-for i in .vimrc .tmux.conf .vim
+for i in .vimrc .dircolors .tmux.conf .vim .bashrc
 do
     ln $params $script_home/$i $HOME/$i
 done
 
 # Install plugins for vim
 vim -E -c PlugInstall -c qall
-export EDITOR=vim
-sed -i '' -e  's/g:dracula_colorterm = 1/g:dracula_colorterm = 0/g' \
+
+# Change to -i '' -e on Mac
+sed -i 's/g:dracula_colorterm = 1/g:dracula_colorterm = 0/g' \
     $script_home/.vim/vim-plug/vim/colors/dracula.vim
 
 
