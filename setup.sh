6f674334eb370d6c1bbbaa8da7d89607e2de5452
Dan Snow <dfsnow@uchicago.edu>
2018-08-09 20:26:00 -0500

    Removed tmux from dotfiles symlinks

--- setup.sh
+++ setup.sh
@@ -30,13 +30,13 @@ params="-sf"
 curl -fLo $script_home/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-git clone https://github.com/gpakosz/.tmux.git .tmux
+git clone https://github.com/gpakosz/.tmux .tmux
 mv .tmux/.tmux.conf ~/.tmux.conf
 tmux source-file ~/.tmux.conf
 rm -rf .tmux
 
 # Create symlinks to all files and folders
-for i in .vimrc .dircolors .tmux.conf .vim .bashrc .inputrc
+for i in .vimrc .dircolors .vim .bashrc .inputrc
 do
     ln $params $script_home/$i $HOME/$i
 done
